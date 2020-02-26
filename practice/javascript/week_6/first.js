a2ae69e6c44a8307fd596820e8d4f7759d7801bc
afiapython <afia.python@gmail.com>
2020-02-26 09:24:26 -0800

    files added

---
+++ practice/javascript/week_6/first.js
@@ -0,0 +1,28 @@
+console.log(10 * 10);
+
+console.log(100 * 100);
+
+console.log(10000);
+
+
+
+console.log("Afia Firdaus");
+
+console.log("100");
+
+console.log("hello" * 100);
+
+console.log("number" * "number");
+
+console.log(100 / 10 * 5);
+
+// comments concatenation
+console.log("how are you" + "afia");
+//the boolean datatype
+console.log(true);
+console.log(false);
+console.log(0);
+console.log(1);
+
+console.log("false");
+
