634a72adc784b6d2ff971014d85aff6e25ffd4a1
afiapython <afia.python@gmail.com>
2020-02-27 10:00:05 -0800

    functions

---
+++ practice/javascript/week_6/functionspractice.js
@@ -0,0 +1,51 @@
+//concatenation example
+
+"a word" + "gabe";
+
+"a number" + 100;
+
+"hello" + true;
+
+console.log("hello" + true);
+
+console.log(100 + "a number");
+
+
+console.log(100 + 100 + 100 + "hello");
+
+console.log("hello" + 100 + 100 + 100);
+
+
+function sentenceBuilder(person, place, action, item) {
+    var firstPart = " went to  ";
+    var secondPart = " , and did ";
+    var thirdPart = " but there was a problem, ";
+    var fourthPart = " did not work. ";
+
+    var completeSentence = place + firstPart + place + action +
+        secondPart + thirdPart + item + fourthPart;
+    console.log(completeSentence);
+}
+
+sentenceBuilder("xxx", "sfo", "owner", "food");
+sentenceBuilder("yyy", "Donuts", "eating", "concord");
+
+
+
+function getNumber() {
+    var theSpecialNumber = 42;
+    return theSpecialNumber;
+
+}
+
+console.log(getNumber());
+
+var newNumber = getNumber() + 100;
+
+console.log(newNumber);
+
+
+
+var results = getNumber() / 50;
+
+console.log(results);
