a77776bcbf3a92fc272fcefac7f32cb2aaaf5dcb
afiapython <afia.python@gmail.com>
2020-03-03 11:22:57 -0800

    practice2.js

---
+++ practice/javascript/week_7/practice2.js
@@ -0,0 +1,29 @@
+var employeeSalaries = [100000, 10000, 1000000, 55000, 75000, 24000, 32000, 40000, 29500, 72000];
+
+
+// update all of the values so we add a 5% bonus if they make
+//less than 50000, or 2.5% if they make more than 50000.
+
+
+for (var i = 0; i < employeeSalaries.length; i++) {
+    console.log(employeeSalaries[i]);
+
+    var salary = employeeSalaries[i];
+
+    if (salary < 50000) {
+        console.log("old salary: " + salary);
+        salary = salary + (salary * .05);
+        console.log("New salary: " + salary);
+
+        employeeSalaries[i] = salary;
+    } else {
+        console.log("old salary: " + salary);
+        salary = salary + (salary * .025);
+        console.log("New salary: " + salary);
+
+        employeeSalaries[i] = salary;
+
+    }
+}
+
+console.log(employeeSalaries)
